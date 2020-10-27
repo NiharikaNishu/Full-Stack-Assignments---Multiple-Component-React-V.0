e5d4045dbf81adde6cd554f948f80c2c1fb0903d
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-10-28 00:19:57 +0530

    chngd

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,50 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false, //variable needed to be changed
+      posi: 0,
+      ballPosition: { left: "0px" }
+    };
+    this.renderChoice = this.renderChoice.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKey = this.handleKey.bind(this);
+  }
 
-        return(
-            <>
-               
-            </>
-        )
+  //call back function
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+  }
+  renderChoice() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else
+      return (
+        <button onClick={this.buttonClickHandler}>Click For One Ball</button>
+      );
+  }
+
+  //bind ArrowRight keydown event
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKey);
+  }
+
+  handleKey(event) {
+    if (event.keyCode === 39) {
+      this.setState({
+        ballPosition: {
+          left: +this.state.ballPosition.left.split("px")[0] + 5 + "px"
+        }
+      });
     }
-}
+  }
 
+  render() {
+    return <div className="playground">{this.renderChoice()}</div>;
+  }
+}
 
 export default App;
